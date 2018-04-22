bcdd2bb9ffb0bcc44be1bfe9e5baca5472862f6f
batica81 <batica@gmail.com>
2018-04-22 13:40:27 +0200

    updates 3

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Express static server with socket.io
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-    
+app.use(express.static('./'));
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 
-io.on('connection', function(socket){
-	io.removeAllListeners();
 
-	setInterval(function () {
-		var d = new Date();
-		io.emit('chat message', d);
-	}, 1000);
+/////////// Serial server
 
-});
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+
+const port = new SerialPort('COM3', {
+// const port = new SerialPort('/dev/ttyUSB0', {
+        baudRate: 9600
+    },
+    function (err) {
+        if (err) {
+            return console.log('Error: ', err.message);
+            // return true;
+        }
+    });
+
+const parser = new Readline();
+
+port.pipe(parser);
+
+parser.on('data', console.log);
+parser.on('data', setCurrentProduct);
+
+let currentProduct = "";
+
+function setCurrentProduct(arg) {
+    currentProduct = arg;
+
+
+    if (currentProduct.substr(0, 4) === 'Page') {
+        io.emit('chat message', currentProduct);
+    }
+
+    // io.on('connection', function (socket) {
+    //     io.removeAllListeners();
+
+        // setInterval(function () {
+        // 	var d = new Date();
+        // 	io.emit('chat message', d);
+        // }, 1000);
+        // io.emit('chat message', currentProduct);
+    // });
+}
