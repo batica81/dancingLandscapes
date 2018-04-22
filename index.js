69d23f038f31420be029b558534bfe5a2940eb02
batica81 <batica@gmail.com>
2018-04-22 17:45:08 +0200

    updates 4

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const opn = require('opn');
+
 
 app.use(express.static('./'));
 
 http.listen(3000, () => console.log('Example app listening on port 3000!'));
 
+opn('http://127.0.0.1:3000', {app: ['chrome']});
 
 
 /////////// Serial server
@@ -18,7 +21,7 @@ const Readline = SerialPort.parsers.Readline;
 
 const port = new SerialPort('COM3', {
 // const port = new SerialPort('/dev/ttyUSB0', {
-        baudRate: 9600
+        baudRate: 115200
     },
     function (err) {
         if (err) {
@@ -31,7 +34,7 @@ const parser = new Readline();
 
 port.pipe(parser);
 
-parser.on('data', console.log);
+// parser.on('data', console.log);
 parser.on('data', setCurrentProduct);
 
 let currentProduct = "";
@@ -39,18 +42,8 @@ let currentProduct = "";
 function setCurrentProduct(arg) {
     currentProduct = arg;
 
-
-    if (currentProduct.substr(0, 4) === 'Page') {
+    if (currentProduct.substr(0, 5) === 'Paylo') {
         io.emit('chat message', currentProduct);
     }
-
-    // io.on('connection', function (socket) {
     //     io.removeAllListeners();
-
-        // setInterval(function () {
-        // 	var d = new Date();
-        // 	io.emit('chat message', d);
-        // }, 1000);
-        // io.emit('chat message', currentProduct);
-    // });
 }
